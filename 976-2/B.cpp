bf61a8dff2946074f72f6cf8482ece7783391a52
101magikarp101 <michaelsxiang@gmail.com>
2024-09-29 12:57:04 -0500

    finished 976-2

---
+++ 976-2/B.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 998244353
+#define MOD2 1000000007
+#define vt vector
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+#define uset unordered_set
+#define umap unordered_map
+#define mset multiset
+struct pii {
+    int x, y;
+    bool operator<(const pii &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pii &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pii &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pii &a) const { return x != a.x || y != a.y; }
+    pii operator+(const pii &a) const { return {x+a.x, y+a.y}; }
+    pii operator-(const pii &a) const { return {x-a.x, y-a.y}; }
+    pii operator*(const int &a) const { return {x*a, y*a}; }
+    pii operator/(const int &a) const { return {x/a, y/a}; }
+};
+struct pll {
+    ll x, y;
+    bool operator<(const pll &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pll &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pll &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pll &a) const { return x != a.x || y != a.y; }
+    pll operator+(const pll &a) const { return {x+a.x, y+a.y}; }
+    pll operator-(const pll &a) const { return {x-a.x, y-a.y}; }
+    pll operator*(const ll &a) const { return {x*a, y*a}; }
+    pll operator/(const ll &a) const { return {x/a, y/a}; }
+};
+
+ll T, N;
+
+ll solve(ll x) {
+    ll sq = sqrt(x);
+    return x-sq;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> T;
+    while (T--) {
+        cin >> N;
+        ll l = 1, r=LLONG_MAX-10;
+        while (l<r) {
+            ll m = (l+r)/2;
+            if (solve(m) >= N) r = m;
+            else l = m+1;
+        }
+        cout << l << endl;
+    }
+    return 0;
+}
---
+++ 976-2/gen.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3")
+using namespace std;
+#define ll long long
+#define MOD 998244353
+#define MOD2 1000000007
+#define vt vector
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+#define uset unordered_set
+#define umap unordered_map
+#define mset multiset
+struct pii {
+    int x, y;
+    bool operator<(const pii &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pii &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pii &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pii &a) const { return x != a.x || y != a.y; }
+    pii operator+(const pii &a) const { return {x+a.x, y+a.y}; }
+    pii operator-(const pii &a) const { return {x-a.x, y-a.y}; }
+    pii operator*(const int &a) const { return {x*a, y*a}; }
+    pii operator/(const int &a) const { return {x/a, y/a}; }
+};
+struct pll {
+    ll x, y;
+    bool operator<(const pll &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pll &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pll &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pll &a) const { return x != a.x || y != a.y; }
+    pll operator+(const pll &a) const { return {x+a.x, y+a.y}; }
+    pll operator-(const pll &a) const { return {x-a.x, y-a.y}; }
+    pll operator*(const ll &a) const { return {x*a, y*a}; }
+    pll operator/(const ll &a) const { return {x/a, y/a}; }
+};
+
+int T, N;
+int a[200005];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    N = 200000;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dis(1, 1023);
+    uniform_int_distribution<int> dis2(1, 10000);
+    T = 1;
+    cout << T << endl;
+    cout << N << endl;
+    for (int i = 0; i < N; i++) {
+        cout << dis(gen) << " ";
+    }
+    cout << endl;
+    for (int i = 0; i < N; i++) {
+        cout << dis2(gen) << " ";
+    }
+    cout << endl;
+    return 0;
+}
