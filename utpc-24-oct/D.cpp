6be00cf4f65405eebda04dc3e306bf6c8c100cfc
101magikarp101 <michaelsxiang@gmail.com>
2024-10-11 20:36:15 -0500

    2nd place utpc 2024-oct

---
+++ utpc-24-oct/D.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 998244353
+#define MOD2 1000000007
+#define vt vector
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+#define uset unordered_set
+#define umap unordered_map
+#define mset multiset
+struct pii {
+    int x, y;
+    bool operator<(const pii &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pii &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pii &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pii &a) const { return x != a.x || y != a.y; }
+    pii operator+(const pii &a) const { return {x+a.x, y+a.y}; }
+    pii operator-(const pii &a) const { return {x-a.x, y-a.y}; }
+    pii operator*(const int &a) const { return {x*a, y*a}; }
+    pii operator/(const int &a) const { return {x/a, y/a}; }
+};
+struct pll {
+    ll x, y;
+    bool operator<(const pll &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pll &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pll &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pll &a) const { return x != a.x || y != a.y; }
+    pll operator+(const pll &a) const { return {x+a.x, y+a.y}; }
+    pll operator-(const pll &a) const { return {x-a.x, y-a.y}; }
+    pll operator*(const ll &a) const { return {x*a, y*a}; }
+    pll operator/(const ll &a) const { return {x/a, y/a}; }
+};
+
+int T, N;
+int a[200005];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string s; cin >> s;
+    vt<int> cnt(26,0);
+    for (int i = 0; i < sz(s); i++) {
+        cnt[s[i]-'a']++;
+    }
+    int odd = -1;
+    for (int i = 0; i < 26; i++) {
+        if (cnt[i]%2) {
+            odd = i;
+        }
+    }
+    string ansl = "", ansr = "";
+    for (int i = 0; i < 26; i++) {
+        ansl += string(cnt[i]/2, 'a'+i);
+        ansr += string(cnt[i]/2, 'a'+i);
+    }
+    if (odd != -1) {
+        ansl += string(cnt[odd], 'a'+odd);
+    }
+    reverse(all(ansr));
+    cout << ansl+ansr << endl;
+    return 0;
+}
---
+++ utpc-24-oct/F.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 998244353
+#define MOD2 1000000007
+#define vt vector
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+#define uset unordered_set
+#define umap unordered_map
+#define mset multiset
+struct pii {
+    int x, y;
+    bool operator<(const pii &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pii &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pii &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pii &a) const { return x != a.x || y != a.y; }
+    pii operator+(const pii &a) const { return {x+a.x, y+a.y}; }
+    pii operator-(const pii &a) const { return {x-a.x, y-a.y}; }
+    pii operator*(const int &a) const { return {x*a, y*a}; }
+    pii operator/(const int &a) const { return {x/a, y/a}; }
+};
+struct pll {
+    ll x, y;
+    bool operator<(const pll &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pll &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pll &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pll &a) const { return x != a.x || y != a.y; }
+    pll operator+(const pll &a) const { return {x+a.x, y+a.y}; }
+    pll operator-(const pll &a) const { return {x-a.x, y-a.y}; }
+    pll operator*(const ll &a) const { return {x*a, y*a}; }
+    pll operator/(const ll &a) const { return {x/a, y/a}; }
+};
+
+ll N, K;
+ll a[500005];
+
+bool solve(ll x) {
+    ll cnt = 0;
+    ll cur = -1e18;
+    for (int i = 0; i < N; i++) {
+        ll d = abs(a[i]-cur);
+        if (d > x) {
+            cur = a[i]+x;
+            cnt++;
+        }
+    }
+    return cnt <= K;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> K;
+    for (int i = 0; i < N; i++) {
+        cin >> a[i];
+    }
+    ll l = 0, r = 1e9;
+    while (l < r) {
+        ll m = (l+r)/2;
+        if (solve(m)) {
+            r = m;
+        } else {
+            l = m+1;
+        }
+    }
+    cout << l << endl;
+    return 0;
+}
