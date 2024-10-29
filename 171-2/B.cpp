46f0c845f8949eab8bebec80cc47454f063c71ab
101magikarp101 <michaelsxiang@gmail.com>
2024-10-29 09:28:06 -0500

    171-2 practice

---
+++ 171-2/B.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define MOD 998244353
+#define MOD2 1000000007
+#define vt vector
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+#define uset unordered_set
+#define umap unordered_map
+#define mset multiset
+#define fi first
+#define se second
+struct pii {
+    int x, y;
+    bool operator<(const pii &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pii &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pii &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pii &a) const { return x != a.x || y != a.y; }
+    pii operator+(const pii &a) const { return {x+a.x, y+a.y}; }
+    pii operator-(const pii &a) const { return {x-a.x, y-a.y}; }
+    pii operator*(const int &a) const { return {x*a, y*a}; }
+    pii operator/(const int &a) const { return {x/a, y/a}; }
+};
+struct pll {
+    ll x, y;
+    bool operator<(const pll &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pll &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pll &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pll &a) const { return x != a.x || y != a.y; }
+    pll operator+(const pll &a) const { return {x+a.x, y+a.y}; }
+    pll operator-(const pll &a) const { return {x-a.x, y-a.y}; }
+    pll operator*(const ll &a) const { return {x*a, y*a}; }
+    pll operator/(const ll &a) const { return {x/a, y/a}; }
+};
+
+template<class T> bool ckmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0; }
+
+int T, N;
+ll a[2005];
+
+bool solve(ll m) {
+    if (N%2==0) {
+        for (int i = 0; i < N; i+=2) {
+            if (a[i+1]-a[i] > m) return 0;
+        }
+        return 1;
+    } else {
+        bool used = 0;
+        for (int i = 0; i < N; i+=2) {
+            if (a[i+1]-a[i] > m) {
+                i--;
+                if (used) return 0;
+                used = 1;
+            }
+        }
+        return 1;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> T;
+    while (T--) {
+        cin >> N;
+        for (int i = 0; i < N; i++) cin >> a[i];
+        ll l = 1, r = 1e18;
+        while (l < r) {
+            ll m = (l+r)/2;
+            if (solve(m)) r = m;
+            else l = m+1;
+        }
+        cout << l << endl;
+    }
+    return 0;
+}
