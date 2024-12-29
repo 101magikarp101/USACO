b57423ab633ca1e3cadc723787e888add9d03da4
101magikarp101 <michaelsxiang@gmail.com>
2024-12-29 13:46:48 -0600

    sus

--- Extra/test1.cpp
+++ Extra/test1.cpp
@@ -1,54 +1,85 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define MOD 998244353
+#define MOD2 1000000007
+#define vt vector
+#define endl '\n'
+#define pb push_back
+#define pf push_front
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)((x).size())
+#define uset unordered_set
+#define umap unordered_map
+#define mset multiset
+#define fi first
+#define se second
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define each(i,a) for(auto &i:a)
+struct pii {
+    int x, y;
+    bool operator<(const pii &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pii &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pii &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pii &a) const { return x != a.x || y != a.y; }
+    pii operator+(const pii &a) const { return {x+a.x, y+a.y}; }
+    pii operator-(const pii &a) const { return {x-a.x, y-a.y}; }
+    pii operator*(const int &a) const { return {x*a, y*a}; }
+    pii operator/(const int &a) const { return {x/a, y/a}; }
+};
+struct pll {
+    ll x, y;
+    bool operator<(const pll &a) const { return x == a.x ? y < a.y : x < a.x; }
+    bool operator>(const pll &a) const { return x == a.x ? y > a.y : x > a.x; }
+    bool operator==(const pll &a) const { return x == a.x && y == a.y; }
+    bool operator!=(const pll &a) const { return x != a.x || y != a.y; }
+    pll operator+(const pll &a) const { return {x+a.x, y+a.y}; }
+    pll operator-(const pll &a) const { return {x-a.x, y-a.y}; }
+    pll operator*(const ll &a) const { return {x*a, y*a}; }
+    pll operator/(const ll &a) const { return {x/a, y/a}; }
+};
+
+template<class T> bool ckmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0; }
+
 using namespace std;
 
-const int MOD = 998244353;
+const int N = 2e5;
 
-int power(int x, int y) {
-    int result = 1;
-    while (y > 0) {
-        if (y % 2 == 1) {
-            result = (1LL * result * x) % MOD;
-        }
-        y = y / 2;
-        x = (1LL * x * x) % MOD;
-    }
-    return result;
-}
-
-int mod_inverse(int x) {
-    return power(x, MOD - 2);
-}
-
-int count_situations(int n, int m, int k) {
-    int fact[n + 1];
-    int inv_fact[n + 1];
-
-    fact[0] = 1;
-    inv_fact[0] = 1;
-
-    for (int i = 1; i <= n; ++i) {
-        fact[i] = (1LL * fact[i - 1] * i) % MOD;
-        inv_fact[i] = mod_inverse(fact[i]);
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
     }
 
-    auto nCr = [&](int n, int r) {
-        return (1LL * fact[n] * inv_fact[r] % MOD * inv_fact[n - r]) % MOD;
-    };
-
-    int result = 0;
-    for (int i = 0; i <= k; ++i) {
-        result = (result + 1LL * nCr(m - 1, i) * power(2, m - i - 1)) % MOD;
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
     }
+};
 
-    return result;
+void insert_numbers(long long x) {
+    clock_t begin = clock();
+    unordered_map<long long, int, custom_hash> numbers;
+
+    for (int i = 1; i <= N; i++)
+        numbers[i * x] = i;
+
+    long long sum = 0;
+
+    for (auto &entry : numbers)
+        sum += (entry.first / x) * entry.second;
+
+    printf("x = %lld: %.3lf seconds, sum = %lld\n", x, (double) (clock() - begin) / CLOCKS_PER_SEC, sum);
 }
 
 int main() {
-    int n, m, k;
-    cin >> n >> m >> k;
-
-    int result = count_situations(n, m, k);
-    cout << result << endl;
-
-    return 0;
+    insert_numbers(107897);
+    insert_numbers(126271);
 }
